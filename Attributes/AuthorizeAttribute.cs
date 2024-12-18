53d05391bb58dd80e78e41ca66d5dcdd3171750c
duong-nc <canhduong29112004@gmail.com>
2024-12-18 15:28:15 +0700

    webgiuaki

---
+++ Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ktra1.Attributes
+{
+    public class AuthorizeAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var maSV = context.HttpContext.Session.GetString("MaSV");
+            if (string.IsNullOrEmpty(maSV))
+            {
+                // Chuyển hướng đến trang đăng nhập nếu không có session "MaSV"
+                context.Result = new RedirectToActionResult("DangNhap", "NguoiDung", null);
+            }
+        }
+    }
+}
